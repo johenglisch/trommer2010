eb64f06932545ec23e56e06f86826836981054d8
Johannes Englisch <johannes.englisch@uni-leipzig.de>
2016-06-09 12:08:46 +0200

    adapted karuk analysis

--- karuk.py
+++ karuk.py
@@ -5,24 +5,22 @@
 from trommer2010 import *
 
 
-karuk = Language(name='Karuk', trans=True)
-
-karuk.morphemes = [
+morphemes = [
     VI('-ap',  [['Acc', '+2', '+pl']])]
 
-karuk.rules = [
+rules = [
     GenRule(['+pl'], [['Nom', '+3'], ['Acc', '+2']])]
 
+
 print('Positive indicative')
-draw_paradigm(karuk)
+
+karuk = Language(morphemes, rules, transitive=True)
+karuk.draw_paradigm()
+
+
+print('Negative')
 
 karuk.rules.extend([
     GenRule(['Acc', '+2'], [['Nom', '-1'], ['-3', '+pl']]),
     GenRule(['Acc', '+2'], [['Nom', '+3']])])
-
-print('Negative')
-draw_paradigm(karuk)
-
-
-def derivation(s):
-    karuk.realise_cell(parse_features(s), verbose=True)
+karuk.draw_paradigm()
